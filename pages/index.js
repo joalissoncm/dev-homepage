778da1de3eb4b2a2cd45644f18cea91f78603d6f
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2023-02-10 23:41:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Home = () => (
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         css={{ backdropFilter: 'blur(10px)' }}
       >
-        Hello, I&apos;m an indie app developer based in Japan!
+        Hello, I&apos;m an Data Scientist based in Brazil!
       </Box>
 
       <Box display={{ md: 'flex' }}>
